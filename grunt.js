613e2c4fb04607ad0ff8827013b2dca4e5626c33
Mark Lussier <mlussier@gmail.com>
2012-09-07 14:00:36 -0700

    updated grunt and package.json to include banner in minified version

--- grunt.js
+++ grunt.js
@@ -1,8 +1,19 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    pkg: '<json:package.json>',
     lint: {
       files: ['js/jquery.dashboard.js']
+    },
+    meta: {
+      banner: '/*\n * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("dd/mm/yyyy") %>\n' +
+        '<%= pkg.homepage ? " * " + pkg.homepage + "\n" : "" %>' +
+        '<%= pkg.origional ? " * " + pkg.origional + "\n" : "" %>' +
+        ' * Copyright (c) 2010-<%= grunt.template.today("yyyy") %> <%= pkg.origionalauthor.name %>\n' +
+        ' * Modifications made by <%= pkg.author.name %>\n' +
+        ' * Dual licensed under the MIT and GPL licenses (same as jQuery):\n'+
+        ' *   <%= _.pluck(pkg.licenses, "url").join("\n *   ") %>\n */'
     },
     watch: {
       files: '<config:lint.files>',
@@ -10,8 +21,8 @@ module.exports = function(grunt) {
     },
     min: {
       dist: {
-        src: ['js/jquery.dashboard.js'],
-        dest: 'js/jquery.dashboard.min.js'
+        src: ['<banner:meta.banner>', 'js/jquery.dashboard.js'],
+        dest: 'js/jquery.dashboard.min-<%= pkg.version %>.js'
       }
     },
     jshint: {
